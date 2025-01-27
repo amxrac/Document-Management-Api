38c38078c15c8d6fbd1927f99fe982a7e7bdab56
an <108365444+amxrac@users.noreply.github.com>
2025-01-27 07:33:53 +0100

    add db context
    create dbcontext class
    add document model to dbcontext

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using DMS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DMS.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Document> Documents { get; set; }
+    }
+}
