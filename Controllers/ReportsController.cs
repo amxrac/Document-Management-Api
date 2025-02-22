69726e043a9221d1cf97499fcd9e512ade5b1ace
an <108365444+amxrac@users.noreply.github.com>
2025-02-22 19:17:20 +0100

    modify userdto. add route to return a list of all users

---
+++ Controllers/ReportsController.cs
@@ -0,0 +1,48 @@
+﻿using DMS.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DMS.DTOs;
+using Microsoft.AspNetCore.Identity;
+using DMS.Models;
+
+namespace DMS.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [ApiController]
+    [Route("api")]
+    public class ReportsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly UserManager<AppUser> _userManager;
+
+        public ReportsController(AppDbContext context, UserManager<AppUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [HttpGet("users")]
+        public async Task<IActionResult> GetUsers()
+        {
+            var usersList = await _context.Users.ToListAsync();
+
+            var usersDto = new List<UserDTO>();
+
+            foreach (var user in usersList)
+            {
+                var roles = await _userManager.GetRolesAsync(user);
+                usersDto.Add(new UserDTO
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Role = roles.FirstOrDefault() 
+                });
+            }
+
+            return Ok(usersDto);
+        }
+    }
+
+}
+
--- DTOs/UserDTO.cs
+++ DTOs/UserDTO.cs
@@ -1,9 +1,8 @@
 ﻿namespace DMS.DTOs
 {
-    public class UsersDTO
+    public class UserDTO
     {
         public string Id { get; set; }
-        public string UserName { get; set; }
         public string Email { get; set; }
         public string Role { get; set; }
     }
