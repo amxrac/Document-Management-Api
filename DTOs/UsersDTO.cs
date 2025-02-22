8a0ade6b07b9226a004a996b5d4d328729eb2290
an <108365444+amxrac@users.noreply.github.com>
2025-02-22 18:43:59 +0100

    add usersdto class

---
+++ DTOs/UsersDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace DMS.DTOs
+{
+    public class UsersDTO
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
